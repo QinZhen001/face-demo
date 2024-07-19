ce17ff857fcfc9b08cbf77fe5a2de04c8b71cbf3
qinzhen <qz519189636@qq.com>
2024-07-19 13:18:53 +0800

    feat: build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,11 @@ const genBaseUrl = (mode) => {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: genBaseUrl(mode),
-  plugins: [react()],
+
+
+export default defineConfig(({ mode }) => {
+  return {
+    base: genBaseUrl(mode),
+    plugins: [react()],
+  }
 })
