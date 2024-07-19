8d95eec808b645203eadc50ce635f6efff3a810b
qinzhen <qz519189636@qq.com>
2024-07-19 11:59:46 +0800

    feat: face

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { name } from "./package.json"
+
+const genBaseUrl = (mode) => {
+  if (mode !== "development") {
+    return `/${name}/`
+  }
+  return "/"
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: genBaseUrl(mode),
   plugins: [react()],
 })
